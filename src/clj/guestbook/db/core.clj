306519d94d4834c810466ccc3001eaaa17e372c1
Hada Ionut <hadaionut@gmail.com>
2017-07-07 13:33:54 +0300

    added improved worktimes

--- src/clj/guestbook/db/core.clj
+++ src/clj/guestbook/db/core.clj
@@ -8,23 +8,3 @@
     [environ.core :as envi]
     [guestbook.config :refer [env]]))
 
-(def debe
-  (db/create-db
-    (db/mysql {:db "ionut"
-            :user "root"
-            :password "whitecityromania"
-            :host "172.17.0.2"
-            :port "3306"})))
-
-(db/default-connection debe)
-
-(defentity BIGBOI)
-(defentity Users)
-
-(select BIGBOI)
-(select Users)
-
-(defn get-messages [] println "test")
-(defn save-message [{:keys [message]}]
-  (insert BIGBOI (values [{:name message}]))
-  println message)
